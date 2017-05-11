4e18fd92b108c94ebb481d23f2e81e25395ccaad
Zonov <crismas90@gmail.com>
2017-05-11 16:57:54 +0300

    landing
    landing

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('a[href^="#"]').on("click", function (e) {
+        var target = $(this).attr('href');
+        var strip = target.slice(1);
+        var anchor = $("a[name='" + strip + "']");
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: anchor.offset().top
+        }, '2000');
+    })
+})
